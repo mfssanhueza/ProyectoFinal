a3fa69e5f2b0926f98bd2aebf53e706689e735e6
mfssanhueza <mfssanhueza@gmail.com>
2018-12-18 20:23:09 -0300

    seeds and faker

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.4.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'devise'
+gem 'faker', group: :development
 gem 'rails', '~> 5.2.2'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |p|
+  Product.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price.to_i,
+    photo: "https://picsum.photos/200/300?image=#{6 + p}",
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.'
+  )
+end
